3e245c384a58006afa6584dcb6b6ff7f14e7d7a4
Dave <davidajberner@gmail.com>
2019-11-20 09:16:31 +1100

    remove redundant test

--- tests/index-test.js
+++
@@ -1,9 +0,0 @@
-// import expect from "expect";
-
-// import lightLevels from "./src";
-
-// describe("Module template", () => {
-//   it("displays a welcome LightLevels", () => {
-//     expect(lightLevels).toContain("Welcome to my-module");
-//   });
-// });
