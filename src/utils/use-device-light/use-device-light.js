651d3c6c522abc872beedfea9aba8cc84ab63d4c
Dave <davidajberner@gmail.com>
2019-11-21 00:21:42 +1100

    tighten up device light api

--- src/utils/use-device-light/use-device-light.js
+++ src/utils/use-device-light/use-device-light.js
@@ -1,13 +1,30 @@
 const createDeviceLightSensor = callback => {
+  const state = {
+    isActive: false
+  };
+
   const handleDeviceLight = e => callback(e.value);
 
-  const start = () => window.addEventListener("devicelight", handleDeviceLight);
-  const stop = () =>
-    window.removeEventListener("devicelight", handleDeviceLight);
-  return {
+  const start = () => {
+    if (!state.isActive) {
+      window.addEventListener("devicelight", handleDeviceLight);
+      state.isActive = true;
+    } else {
+      console.warn("This instance of LightLevels is already running");
+    }
+  };
+  const stop = () => {
+    if (state.isActive) {
+      window.removeEventListener("devicelight", handleDeviceLight);
+      state.isActive = false;
+    } else {
+      console.warn("This instance of LightLevels has already been stopped");
+    }
+  };
+  return Object.freeze({
     start,
     stop
-  };
+  });
 };
 
 const useDeviceLight = callback => {
