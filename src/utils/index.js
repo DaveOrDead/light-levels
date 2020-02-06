70d441e487581cd5c1c217b92dca08e9d1cd25c5
daveordead <davidajberner@gmail.com>
2020-02-06 20:25:06 +1100

    update imports for UMD modules

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,6 @@
-export { default as getLuxName } from "./getLuxName";
-export { default as removeClassesWithPrefix } from "./removeClassesWithPrefix";
-export { default as setLightLevel } from "./setLightLevel";
-export { default as setLightLevelClasses } from "./setLightLevelClasses";
-export { default as useAmbientLightSensor } from "./useAmbientLightSensor";
-export { default as useDeviceLight } from "./useDeviceLight";
+export { default as getLuxName } from "./getLuxName/getLuxName";
+export { default as removeClassesWithPrefix } from "./removeClassesWithPrefix/removeClassesWithPrefix";
+export { default as setLightLevel } from "./setLightLevel/setLightLevel";
+export { default as setLightLevelClasses } from "./setLightLevelClasses/setLightLevelClasses";
+export { default as useAmbientLightSensor } from "./useAmbientLightSensor/useAmbientLightSensor";
+export { default as useDeviceLight } from "./useDeviceLight/useDeviceLight";
